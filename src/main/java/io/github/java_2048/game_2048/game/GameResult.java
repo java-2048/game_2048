000e80ba37a1c30d53bcbab326b1dea070ab1af9
found-cake <skymin468@gmail.com>
2023-12-08 18:12:42 +0900

    Wrapper 사용하기

--- src/main/java/io/github/java_2048/game_2048/game/GameResult.java
+++ src/main/java/io/github/java_2048/game_2048/game/GameResult.java
@@ -2,8 +2,8 @@ package io.github.java_2048.game_2048.game;
 
 // 게임 종료 처리에 사용될 record class
 public record GameResult(
-		boolean isWin, // 승리 여부
-		int goal, // 목표 수치
-		int score, // 점수
-		int move // 이동 횟수
+		Boolean isWin, // 승리 여부
+		Integer goal, // 목표 수치
+		Integer score, // 점수
+		Integer move // 이동 횟수
 ) { }
